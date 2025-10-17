8bc4ec11caf8f57bd2d91431d3c9abe8bc5d3440
lby-magicbook <1508201988@qq.com>
2025-10-17 13:33:57 +0800

    feat：简单调用

--- src/main/java/com/lbytech/App.java
+++ src/main/java/com/lbytech/App.java
@@ -1,13 +1,24 @@
 package com.lbytech;
 
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // 读取环境配置
+        String apiKey = System.getenv("openai.api.key");
+
+        // 构建OpenAiChatModel对象
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
+                .modelName("qwen-plus")
+                .apiKey(apiKey)
+                .build();
+
+        // 调用模型
+        String response = model.chat("博哥帅不帅");
+        System.out.println(response);
     }
 }

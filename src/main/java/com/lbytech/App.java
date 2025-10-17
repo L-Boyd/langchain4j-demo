041bdc6994ce67ad8a4c2da1b8421025c884d85d
lby-magicbook <1508201988@qq.com>
2025-10-17 13:36:27 +0800

    feat：打印调用日志

--- src/main/java/com/lbytech/App.java
+++ src/main/java/com/lbytech/App.java
@@ -2,9 +2,7 @@ package com.lbytech;
 
 import dev.langchain4j.model.openai.OpenAiChatModel;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
         // 读取环境配置
@@ -15,6 +13,8 @@ public class App {
                 .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
                 .modelName("qwen-plus")
                 .apiKey(apiKey)
+                .logRequests(true)
+                .logResponses(true)
                 .build();
 
         // 调用模型
